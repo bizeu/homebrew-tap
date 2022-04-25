92210902a44eccac818bf5e62e79ac18a2ad8fd8
root <root@example.com>
2022-04-25 14:20:03 +0200

    auto

--- Formula/shts.rb
+++ Formula/shts.rb
@@ -1,40 +1,30 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+# typed: false
+# frozen_string_literal: true
+
 class Shts < Formula
+  formula=name.demodulize
+  github="https://github.com/bizeu/#{formula}"
+  sha256 "e46ab1db43e9ee97028ce3b6347d839727c3ac7d0ed61c031afae84126c6871d"
   desc ""
-  homepage ""
-  license ""
-  head "https://github.com/bizeu/shts"
+  homepage github.to_s
+  license "MIT"
+  head github.to_s, { branch: "main" }
   
   depends_on "bash"
   depends_on "bash-completion@2"
   depends_on "bats-core" => :keg_only
-  depends_on "bats-core/bats-core/bats-assert"
-  depends_on "bats-core/bats-core/bats-file"
-  depends_on "bats-core/bats-core/bats-support"
+  depends_on "bats-core/bats-core/bats-assert" => :keg_only
+  depends_on "bats-core/bats-core/bats-file" => :keg_only
+  depends_on "bats-core/bats-core/bats-support" => :keg_only
   depends_on "parallel" if OS.mac?
 
-  # depends_on "cmake" => :build
-
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install mierda.sh
+    bin.install Dir["bin/*"]
+    bash_completion.install Dir["etc/bash_completion.d/*"]
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test shts`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "true"
+    system "#{HOMEBREW_PREFIX}/bin/#{name}", "--help"
   end
 end
