02cf092d11c72b6c7eb9ace260bc9e4a166a0024
root <root@example.com>
2022-05-10 03:22:47 +0200

    JetBrains: links, scripts

---
+++ cmd/links.rb
@@ -0,0 +1,29 @@
+# typed: false
+# frozen_string_literal: true
+
+require 'cli/parser'
+require 'formula'
+
+require_relative '../lib/functions'
+require_relative '../lib/jetbrains'
+
+module Homebrew
+  extend T::Sig
+
+  module_function
+
+  sig { returns(CLI::Parser) }
+
+  def links_args
+    Homebrew::CLI::Parser.new do
+      description <<~GRC_DESC
+        Patches grc.
+      GRC_DESC
+    end
+  end
+
+  def links
+    JetBrains.links 
+  end
+end
+
